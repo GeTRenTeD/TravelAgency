8cfe95794d95216e6ac3683f054a10774e78deea
Aleksey Savelev <114014925+GeTRenTeD@users.noreply.github.com>
2024-02-29 14:38:45 +0300

    feat: pdf generator

--- frontend/src/hooks/useFetch.js
+++ frontend/src/hooks/useFetch.js
@@ -6,11 +6,11 @@ const useFetch = url => {
   const [data, setData] = useState([]);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
-  const dispatch = useDispatch(); // Получаем функцию диспетчера из React Redux
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const fetchData = async () => {
-      dispatch(showLoading()); // Диспетчируем экшен для показа состояния загрузки
+      dispatch(showLoading()); 
       setLoading(true);
       try {
         const res = await fetch(url);
--- frontend/src/pages/Tours.jsx
+++ frontend/src/pages/Tours.jsx
@@ -1,31 +1,53 @@
 import React, { useState, useEffect } from "react";
 import CommonSection from "../shared/CommonSection";
-
 import "../styles/tour.css";
 import TourCard from "./../shared/TourCard";
 import SearchBar from "./../shared/SearchBar";
 import Newsletter from "./../shared/Newsletter";
 import { Container, Row, Col } from "reactstrap";
-
 import useFetch from "../hooks/useFetch";
 import { BASE_URL } from "../utils/config";
+import jsPDF from "jspdf";
 
 const Tours = () => {
   const [pageCount, setPageCount] = useState(0);
   const [page, setPage] = useState(0);
+  const [toursData, setToursData] = useState([]);
 
-  const {
-    data: tours,
-    loading,
-    error,
-  } = useFetch(`${BASE_URL}/tours?page=${page}`);
+  const { data: tours, loading, error } = useFetch(`${BASE_URL}/tours?page=${page}`);
   const { data: tourCount } = useFetch(`${BASE_URL}/tours/search/getTourCount`);
 
   useEffect(() => {
     const pages = Math.ceil(tourCount / 8);
     setPageCount(pages);
+    setToursData(tours); //данНые о турах в состояние
     window.scrollTo(0, 0);
   }, [page, tourCount, tours]);
+
+  const handleDownloadPDF = () => {
+    const doc = new jsPDF();
+  
+    let y = 10;
+    toursData.forEach((tour, index) => {
+      if (index !== 0) {
+      }
+
+      doc.addImage(tour.photo, "JPEG", 10, y, 80, 60);
+  
+      doc.setFontSize(16);
+      doc.text(`Tour ${index + 1}: ${tour.title}`, 100, y + 10);
+  
+      doc.setFontSize(12);
+      doc.text(`City: ${tour.city}`, 100, y + 20);
+  
+      doc.text(`Price: $${tour.price} per person`, 100, y + 30);
+  
+      y += 80; //высота карточки плюс отступ
+    });
+  
+    doc.save("tours_list.pdf");
+  };
+  
 
   return (
     <>
@@ -35,6 +57,22 @@ const Tours = () => {
           <Row>
             <SearchBar />
           </Row>
+          <button
+              className="pdfCreateButton"
+              style={{
+                background: "var(--secondary-color)",
+                color: "white",
+                cursor: "pointer",
+                padding: "0.5rem 0.5rem",
+                marginTop: "1rem",
+                marginLeft: "1rem",
+                borderRadius: "5px",
+                border: "none",
+              }}
+              onClick={handleDownloadPDF} //оБработчик для скачивания PDF
+            >
+              All tours to PDF
+            </button>
         </Container>
       </section>
       <section className="pt-0">
@@ -43,15 +81,14 @@ const Tours = () => {
           {error && <h4 className="text-center pt-5">{error}</h4>}
           {!loading && !error && (
             <Row>
-              {tours?.map(tour => (
+              {tours?.map((tour) => (
                 <Col lg="3" md="6" sm="6" className="mb-4" key={tour._id}>
                   <TourCard tour={tour} />
                 </Col>
               ))}
-
               <Col lg="12">
                 <div className="pagination d-flex align-items-center justify-content-center mt-4 gap-3">
-                  {[...Array(pageCount).keys()].map(number => (
+                  {[...Array(pageCount).keys()].map((number) => (
                     <span
                       key={number}
                       onClick={() => setPage(number)}
@@ -72,3 +109,4 @@ const Tours = () => {
 };
 
 export default Tours;
+
